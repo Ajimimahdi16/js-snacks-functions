2cd437cec588fbf47cb7170a3c4733d8b97b0d40
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2025-12-16 18:56:10 +0100

    esercizio coretto dopo la lezione con il techers riccardo

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,15 +1,17 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
-function nomeCittaino(userName) {
-  return `Ciao ${userName}`;
+function nomeCittaino(Name) {
+  return `Ciao ${Name}`;
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
-//Risultato atteso se si passa 'Mario':
-
 console.log(nomeCittaino(userName));
+//Risultato atteso se si passa 'Mario':// ciao mario
+
+
+
 
 
 
