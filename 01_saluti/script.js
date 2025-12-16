816f9f7ef861107413d3592516a15598b017a4d9
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2025-12-16 17:59:08 +0100

    inseriemento il nome del cittadino mario con il ritorno della funzione in ciao mario con la stampa quindi il richiamo della funzione stampato ciao mario

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
-
-
-// Dichiara la funzione qui.
+function nomeCittaino(userName) {
+  return `Ciao ${userName}`;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+//Risultato atteso se si passa 'Mario':
+
+console.log(nomeCittaino(userName));
 
 
 
-//Risultato atteso se si passa 'Mario': // ciao Mario
+
+
+
+
