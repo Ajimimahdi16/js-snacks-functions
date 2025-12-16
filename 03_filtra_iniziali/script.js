96b7a7dd958a0145992068dae6dde229315b1e64
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2025-12-16 18:59:35 +0100

    eseguendo lo stesso metodo dell'esercizio precedente usiamo la proprieta Array inserendo solo le inziali di ogni parola con toLoweCase

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,9 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtraIniziali(namesArray, letter) {
+  const filteredNames = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    if (namesArray[i][0].toLowerCase() === letter.toLowerCase()) {
+      filteredNames.push(namesArray[i]);
+    }
+  }
+  return filteredNames;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(filtraIniziali(names, 'A'));
 
 
 
